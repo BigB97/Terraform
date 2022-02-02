09c3d39dae47d4c743e720ca013855e94630864e
BigB97 <bigbofficial01@gmail.com>
2022-02-02 14:54:12 +0100

    terraform update

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
+
+variable "AWSAccessKey" { description = var.environments }
+variable "AWSRegion" { description = var.environments}
+variable "AWSSecret" {description = var.environments}
+variable "cidr_block" {description = var.env }
+variable "SubnetRegion" {description = "dev"}
   provider "aws" {
-    region     = "eu-west-2"
-    access_key = "AKIAXA76NSHNR2YOTXN5"
-    secret_key = "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"
+    region = var.AWSRegion
+    access_key = var.AWSAccessKey
+    secret_key = var.AWSSecret
   }
 
   resource "aws_vpc" "dev_vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.cidr_block
         tags = {
       Name = "Dev_Vpc_terra" 
     }
@@ -13,21 +19,25 @@
 
   resource "aws_subnet" "dev_subnet1" {
     vpc_id            = aws_vpc.dev_vpc.id
-    cidr_block        = "10.0.10.0/24"
-    availability_zone = "eu-west-2a"
+    cidr_block        = var.cidr_block
+    availability_zone = var.SubnetRegion
     tags = {
       Name = "subnet-dev-terra1" 
     }
   }
-  data "aws_vpc" "existing_vpc" {
-    default = true
-  }
+#   data "aws_vpc" "existing_vpc" {
+#     default = true
+#   }
 
-  resource "aws_subnet" "dev_subnet2" {
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "eu-west-2a"
-    tags = {
-      Name = "subnet-dev-terra2"
-    }
-  }
+#   resource "aws_subnet" "dev_subnet2" {
+#     vpc_id = data.aws_vpc.existing_vpc.id
+#     cidr_block = "172.31.48.0/20"
+#     availability_zone = "eu-west-2a"
+#     tags = {
+#       Name = "subnet-dev-terra2"
+#     }
+#   }
+
+# output "dev_subnet" {
+#   value =  aws_vpc.dev_vpc.id
+# }
